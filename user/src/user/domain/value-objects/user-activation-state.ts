cc32d64239a7e6683c7101481ded13cb74b446b0
hosseinhashemi <hossein.hashhemi@gmail.com>
2024-11-04 00:10:54 +0330

    Finish

--- user/src/user/domain/value-objects/user-activation-state.ts
+++
@@ -1,7 +0,0 @@
-export class UserActivationState {
-  constructor(readonly value: 'active' | 'deactive' | 'deleted') {}
-
-  equals(activationState: UserActivationState) {
-    return this.value === activationState.value;
-  }
-}
